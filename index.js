2c5a808ffec63a8e1b94dabe781a5a07818275fd
BrettSWeisberg <weisberg.brett@gmail.com>
2018-11-22 10:29:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
+
+function nestedTarget() {
+  document.querySelector('div.target')
+}
+
+function increaseRankBy(n){
+   document.querySelectorAll('ul.ranked-list li');
+
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + 1).toString();
+}
+}
+
+//
+// <div id="nested">
+//   <div>
+//     <div>
+//       <div>
+//         <div class="target">
+//           1
